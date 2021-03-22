2e06ea55e2ec60ee9c91b7f4fde293c5d3e67956
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2021-03-22 16:08:45 +0530

    Added demo time.py program

---
+++ time.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+import pytz
+
+tz_PST = pytz.timezone('US/Pacific') 
+datetime_PST = datetime.now(tz_PST)
+print("PST:", datetime_PST.strftime("%H:%M:%S"))
+
+tz_IST = pytz.timezone('Asia/Kolkata')
+datetime_IST = datetime.now(tz_IST)
+print("IST:", datetime_IST.strftime("%H:%M:%S"))
